c9946ef8088c8bd0ad885a3f2a4324e39ed3fdae
modar_algayal <mudar.algayal@helsinki.fi>
2024-10-01 19:42:30 +0300

    Working form addition with linked clientId

--- src/controllers/formController.js
+++ src/controllers/formController.js
@@ -2,7 +2,8 @@ import { scanTable, addItemToTable } from "../services/dynamoServices.js";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import { v4 as uuidv4 } from 'uuid';
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { v4 as uuidv4 } from "uuid";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,26 +12,44 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 export const addApplicationForm = async (req, res) => {
   try {
-    const userId = req.user; // Get user information from the JWT
-    const application = req.body.application; // Get application data from the request body
+    const userId = req.user;
+    const application = req.body.application;
 
     console.log("This is user Id: ", userId);
     console.log("This is the application: ", application);
 
-    // Create a new item for the DynamoDB table
     const Item = {
-      id: uuidv4(), // Generate a unique ID for this application
-      username: userId.username, // Assuming userId is an object with a username property
-      clientId: userId.clientId, // Assuming userId includes a clientId property as well
+      id: uuidv4(),
+      username: userId.username,
+      clientId: userId.clientId,
       Applications: application,
-      createdAt: new Date().toISOString(), // Optionally add a timestamp for when the application was created
+      createdAt: new Date().toISOString(),
     };
 
-    await addItemToTable(Item); // Call the function to add the item to the table
+    await addItemToTable(Item);
 
     res.status(201).json({ message: "Application form added successfully!" });
   } catch (error) {
     console.error("Error adding application form:", error);
     res.status(500).json({ error: "Failed to add application form" });
+  }
+};
+// Untested, Test it later
+export const getItemsByClientId = async (clientId) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    FilterExpression: "clientId = :clientId",
+    ExpressionAttributeValues: {
+      ":clientId": clientId,
+    },
+  };
+
+  try {
+    const command = new ScanCommand(params); 
+    const data = await client.send(command);
+    return data.Items;
+  } catch (error) {
+    console.error("Error fetching items by clientId:", error);
+    throw new Error("Failed to fetch items");
   }
 };
